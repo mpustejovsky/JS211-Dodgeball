be0e62eaacf4f16b5e4d8bd2668f37bd749392e9
mpustejovsky <mpustejovsky@yahoo.com>
2020-08-08 15:41:10 -0500

    added code plan

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # JS211-Dodgeball
+Project Scope:
+20pts - Code Plan - Include this in a README.md file in your folder with comment in your code.
+20pts - Can add People to Players - When clicked the people are added to the Players column and removed from the People list while getting new values of a player added to them.
+20pts - Can add Players to different Teams - When we click on the blue button the Player is added to the blue team and removed from the Player list while also getting the keys color and mascot extended to them when they are moved to a team.
+20pts - Uses Class - This is not a hack job. You should use class to add the new properties you need and extend when you need.
+20pts - Minimum 3 Unit Tests - Use Mocha and Chai to give us at least 3 unit test that prove a person becomes a player and player becomes a teammate.
+
+Code Plan:
+
+
+1.  Make a class for a player that can use the information from the arrayOfPeople.
+2.  Make an input form on the Dom that asks the necassary questions to input the registration information when moving a player from the arrayofPeople to the new player class.
+3.  Create an area to show all registered player, with a button that allows them to pick the team they are to be placed on.
+4.  When the button for "pick a team" is clicked, have a form that allows them to pick the team they want.  when the team is picked assign them to either the red team array or blue team.  Also give them keys with team name and mascot at this time in their array.
+5.  Create a button for each player in three registered player areas (Resitstered Players, Red Team, or Blue team) that when pushed shows all the players related info.
+6.  Create a button in the "List of Peoples area" that ties to a form to allow new people to be entered in the system.
+7.  Make players that are not healthy show up in red.
+8.  Add mascot and team colors displayed in the list area for the team.
+9.  Use grid layout to create two columns of data on the DOM.  Have List of people, blue team, and red team in one column.  Have registered player and player info int he other column.
---
+++ main.js
@@ -0,0 +1,373 @@
+const arrOfPeople = [
+  {
+    id: 2,
+    name: "Charles Young",
+    age: 55,
+    skillSet: "welding",
+    placeBorn: "Omaha, Nebraska"
+  },
+  {
+    id: 3,
+    name: "Judy Twilight",
+    age: 35,
+    skillSet: "fishing",
+    placeBorn: "Louisville, Kentucky"
+  },
+  {
+    id: 4,
+    name: "Cynthia Doolittle",
+    age: 20,
+    skillSet: "tic tac toe",
+    placeBorn: "Pawnee, Texas"
+  },
+  {
+    id: 5,
+    name: "John Willouby",
+    age: 28,
+    skillSet: "pipe fitting",
+    placeBorn: "New York, New York"
+  },
+  {
+    id: 6,
+    name: "Stan Honest",
+    age: 20,
+    skillSet: "boom-a-rang throwing",
+    placeBorn: "Perth, Australia"
+  },
+  {
+    id: 7,
+    name: "Mia Watu",
+    age: 17,
+    skillSet: "acrobatics",
+    placeBorn: "Los Angeles, California"
+  },
+  {
+    id: 8,
+    name: "Walter Cole",
+    age: 32,
+    skillSet: "jump rope",
+    placeBorn: "New Orleans, Louisiana"
+  },
+]
+let registeredIndexs = []
+let listOfPlayers = []
+let blueTeam = []
+let redTeam = []
+let newId = 9;
+
+class player {
+  constructor(id, name, age, skillSet, placeBorn) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.skillSet = skillSet;
+    this.placeBorn = placeBorn;
+    this.canThrowBall;
+    this.canDodgeBall;
+    this.hasPaid;
+    this.isHealthy;
+    this.yearsExperience;
+    this.skillSetScore;
+  }
+}
+class blueTeammate {
+  constructor() { }
+}
+class redTeammate {
+
+  constructor() { }
+}
+
+const listPeopleChoices = (add = 0) => {
+  if (add == 0) { clearList("people") }
+  const listElement = document.getElementById('people')
+  if (add == 1) { //only adds 1 person if function passed a 1
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+    button.id = `butt${arrOfPeople[arrOfPeople.length - 1].id}`;
+    button.innerHTML = "Register Player"
+    button.addEventListener('click', function () {
+      makePlayer(arrOfPeople[arrOfPeople.length - 1].id)
+      document.getElementById(`butt${arrOfPeople[arrOfPeople.length - 1].id}`).style.display = 'none';
+
+    })
+    li.appendChild(button)
+    let indexTemp = arrOfPeople.length - 1
+
+    li.appendChild(document.createTextNode(arrOfPeople[arrOfPeople.length - 1].name + " - " + arrOfPeople[arrOfPeople.length - 1].skillSet))
+    listElement.append(li)
+  }
+
+  else {
+    arrOfPeople.map(person => {
+      const li = document.createElement("li")
+      const button = document.createElement("button")
+      button.id = `butt${person.id}`;
+      button.innerHTML = "Register Player"
+      button.addEventListener('click', function () {
+        makePlayer(person.id)
+        document.getElementById(`butt${person.id}`).style.display = 'none';
+
+      })
+      li.appendChild(button)
+      li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+      listElement.append(li)
+
+    })
+  }
+  if (add == 0) { document.getElementById('listPlay').style.display = "none"; }
+
+
+}
+
+const makePlayer = (idx) => {
+  console.log(`li ${idx} was clicked!`)
+  console.log(registeredIndexs)
+
+
+
+  if (registeredIndexs.indexOf(idx) != -1) {
+    alert("already registered")
+    return;
+  }
+
+
+
+
+  let newPlayerIndex;
+  for (let i = 0; i < arrOfPeople.length; i++) {
+    if (arrOfPeople[i].id == idx) { newPlayerIndex = i }
+  }
+  registeredIndexs.push(idx);
+
+
+  listOfPlayers[listOfPlayers.length] = new player(arrOfPeople[newPlayerIndex].id, arrOfPeople[newPlayerIndex].name, arrOfPeople[newPlayerIndex].age, arrOfPeople[newPlayerIndex].skillSet, arrOfPeople[newPlayerIndex].placeBorn);
+
+  getRegistrationData(listOfPlayers.length - 1);
+
+
+  console.log(listOfPlayers)
+
+}
+
+const displayDBPlayers = () => {
+  clearList('players');
+  const listElement = document.getElementById('players');
+  listOfPlayers.map(person => {
+    const li = document.createElement("li")
+    li.id = `dodgePerson${person.id}`
+    console.log(li.id)
+
+    const button = document.createElement("button")
+    button.innerHTML = "Pick Team"
+    button.addEventListener('click', function () {
+      console.log("person.id=" + person.id)
+      choseTeam(person.id)
+    })
+    li.appendChild(button)
+    li.appendChild(document.createTextNode(person.name + ":Skill Sore= " + person.skillSetScore))
+    listElement.append(li)
+    //info button
+
+    const button2 = document.createElement("button")
+    button2.className = "fa fa-folder"
+    button2.addEventListener('click', function () {
+      showInfo(listOfPlayers, person.id)
+    })
+    li.appendChild(button2)
+
+
+    console.log(li.id)
+    if (person.isHealthy == "Injured") { document.getElementById(li.id).style.color = "red" }
+
+  })
+
+
+}
+
+
+
+
+const clearList = (id) => {
+  let parent = document.getElementById(id);
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+// Modal functions below
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+const getRegistrationData = (idx) => {
+  modal.style.display = "block";
+  document.getElementById('fname').value = idx //passing index to form so can use on submit button
+}
+
+const dodgeSkills = () => {
+  let registeredID;
+  let formInfo = document.getElementById("frm1");
+  registeredID = formInfo.elements[5].value;
+  listOfPlayers[registeredID].canThrowBall = formInfo.elements[0].value;
+  listOfPlayers[registeredID].canDodgeBall = formInfo.elements[1].value;
+  listOfPlayers[registeredID].hasPaid = formInfo.elements[2].value;
+  listOfPlayers[registeredID].isHealthy = formInfo.elements[3].value;
+  listOfPlayers[registeredID].yearsExperience = formInfo.elements[4].value;
+  sum = 0;
+  for (let i = 0; i < 5; i++) {
+    if ((i == 0) || (i == 1) || (i == 1)) {
+      sum = sum + parseInt(formInfo.elements[i].value)
+      console.log(sum)
+    }
+  }
+
+  sum = sum / 3;
+  let average = sum.toFixed(1)
+  listOfPlayers[registeredID].skillSetScore = average;
+  console.log(listOfPlayers[registeredID])
+  modal.style.display = "none";
+  displayDBPlayers();
+
+
+
+}
+
+const displayRedPlayers = () => {
+
+  console.log("made it to red")
+  clearList('red');
+  const listElement = document.getElementById('red');
+  redTeam.map(person => {
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(person.name + ":Skill Sore= " + person.skillSetScore))
+    listElement.append(li)
+    li.id = `redperson${person.id}`
+    if (person.isHealthy == "Injured") { document.getElementById(li.id).style.color = "red" }
+    const button = document.createElement("button")
+    button.className = "fa fa-folder"
+    button.addEventListener('click', function () {
+      showInfo(redTeam, person.id)
+    })
+    li.appendChild(button)
+  })
+}
+
+const choseTeam = (id) => {
+  modal2.style.display = "block";
+  console.log("id====" + id)
+  document.getElementById('fname2').value = id //passing ID to form in hidden value 
+  console.log("chose team")
+}
+
+const moveToTeam = () => {
+  modal2.style.display = 'none'
+
+  let formInfo = document.getElementById("frm2");
+  let team = formInfo.elements[0].value;
+  let idx = formInfo.elements[1].value;
+  let index;
+  index = findWithAttr(listOfPlayers, "id", idx)
+  if (team == "Blue") { blueTeam.push(listOfPlayers[index]) }
+  else { redTeam.push(listOfPlayers[index]) }
+  console.log(listOfPlayers)
+  listOfPlayers.splice(index, 1)
+  console.log(listOfPlayers)
+  if (blueTeam.length > 0) { displayBluePlayers(); }
+  if (redTeam.length > 0) { displayRedPlayers(); }
+  displayDBPlayers();
+
+}
+
+const displayBluePlayers = () => {
+
+
+  clearList('blue');
+  const listElement = document.getElementById('blue');
+  blueTeam.map(person => {
+    const li = document.createElement("li")
+    li.id = `blueperson${person.id}`
+    li.appendChild(document.createTextNode(person.name + ":Skill Sore= " + person.skillSetScore))
+
+    listElement.append(li)
+    if (person.isHealthy == "Injured") { document.getElementById(li.id).style.color = "red" }
+    const button = document.createElement("button")
+    button.className = "fa fa-folder"
+    button.addEventListener('click', function () {
+      showInfo(blueTeam, person.id)
+    })
+    li.appendChild(button)
+  })
+}
+
+
+
+//function to find index of key with value
+function findWithAttr(array, attr, value) {
+  console.log(value)
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(array[i][attr])
+    if (array[i][attr] == value) {
+      console.log(i)
+      return i;
+    }
+  }
+  return -1;
+}
+
+const showInfo = (array, idx) => {
+
+  let index = findWithAttr(array, "id", idx)
+
+  clearList('user-info');
+  const listElement = document.getElementById('user-info');
+  console.log(array)
+  console.log(index)
+  const keys = Object.keys(array[index])
+  for (let i = 0; i < 11; i++) {
+    const li = document.createElement("li")
+    li.appendChild(document.createTextNode(keys[i] + ": " + array[index][keys[i]]))
+    listElement.append(li)
+  }
+
+
+}
+
+const addPerson = () => {
+  modal3.style.display = "block";
+  console.log("Ready to add")
+
+
+}
+
+
+const addPersonInfo = () => {
+  modal3.style.display = 'none'
+  let formInfo = document.getElementById("frm3");
+
+  arrNum = arrOfPeople.length
+  arrOfPeople[arrNum] = {};
+  arrOfPeople[arrNum].id = newId
+  arrOfPeople[arrNum].name = formInfo.elements[0].value;
+  arrOfPeople[arrNum].age = formInfo.elements[1].value;
+  arrOfPeople[arrNum].skillSet = formInfo.elements[2].value;
+  arrOfPeople[arrNum].placeBorn = formInfo.elements[3].value;
+  console.log(arrOfPeople)
+  newId++;
+  listPeopleChoices(1); //pass function a 1 to add only 1 person and not the whole list
+
+}
